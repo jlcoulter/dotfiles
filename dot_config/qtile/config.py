f65da37524e6b8b3ac81188b043a7f03004f27df
Jack Coulter <coulter.l.jack@gmail.com>
2022-06-03 20:54:00 +1000

    Update dot_config/qtile/__pycache__/config.cpython-310.pyc Update dot_config/qtile/config.py

--- dot_config/qtile/config.py
+++ dot_config/qtile/config.py
@@ -165,12 +165,12 @@ screens = [
                 ),
                 widget.Clock(
                     format="%Y-%m-%d %a %I:%M %p",
-                    padding = 2150
+                    padding = 2000
                     ),
             ],
             24,
             border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            #border_color=["ff00ff", "000000", "ff00ff", "000000"],  # Borders are magenta
+            border_color=["000000", "000000", "000000", "000000"],  # Borders are magenta
             background="#50000000"
         ),
     ),
